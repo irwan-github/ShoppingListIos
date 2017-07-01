8d8accaee04e7298f006cdf8606ce1653f835479
Mirza Irwan <pembinabimbit@Roys-iMac.local>
2017-07-01 21:04:27 +0800

    Add method to search or create new shopping list instance. Add method load dummy shopping list.

--- Shopping List/ShoppingList.swift
+++ Shopping List/ShoppingList.swift
@@ -11,4 +11,54 @@ import CoreData
 
 class ShoppingList: NSManagedObject {
     
+    //Helper initialize
+    
+    static let entityName = "ShoppingList"
+    
+    convenience init(name: String, comments: String, insertInto context: NSManagedObjectContext) {
+        
+        //Returns the entity with the specified name from the managed object model associated with the specified managed object context’s persistent store coordinator.
+        let shoppingListEntity = NSEntityDescription.entity(forEntityName: ShoppingList.entityName, in: context)
+        
+        if let shoppingListEntity = shoppingListEntity {
+            self.init(entity: shoppingListEntity, insertInto: context)
+            self.name = name
+            self.comments = comments
+            self.lastUpdatedOn = NSDate()
+        } else {
+            fatalError("Entity ShoppingList not found")
+        }
+    }
+    
+    class func findOrCreateNew(name: String, context: NSManagedObjectContext) -> ShoppingList? {
+        
+        var shoppingList: ShoppingList? = nil
+        
+        //Request Shopping List
+        let shoppingListRequest: NSFetchRequest<ShoppingList> = ShoppingList.fetchRequest()
+        
+        //Matching criteria
+        let criteria = NSPredicate(format: "name = %@", name)
+        shoppingListRequest.predicate = criteria
+        
+        //Fetch
+        do {
+            let shoppingLists = try context.fetch(shoppingListRequest)
+            
+            if shoppingLists.count == 1 {
+                shoppingList = shoppingLists[0]
+                //print(">>>Fetch \(shoppingList?.isInserted)")
+            } else {
+                shoppingList = ShoppingList(context: context)
+                //print(">>>Instantiate \(shoppingList?.isInserted)")
+            }
+        } catch {
+            let error = error as NSError
+            fatalError("Error is \(error.userInfo)")
+            
+        }
+        
+        return shoppingList
+        
+    }
 }
