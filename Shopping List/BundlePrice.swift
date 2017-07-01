a471ab8121becc5c7531c237b4178b231efec241
Mirza Irwan <pembinabimbit@Roys-iMac.local>
2017-07-01 10:56:47 +0800

    Create core data model and class.

---
+++ Shopping List/BundlePrice.swift
@@ -0,0 +1,14 @@
+//
+//  BundlePrice.swift
+//  Shopping List
+//
+//  Created by Mirza Irwan on 1/7/17.
+//  Copyright © 2017 Mirza Irwan <mirza.irwan.osman@gmail.com>. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class BundlePrice: NSManagedObject {
+
+}
---
+++ Shopping List/ShoppingList.swift
@@ -0,0 +1,14 @@
+//
+//  ShoppingList.swift
+//  Shopping List
+//
+//  Created by Mirza Irwan on 1/7/17.
+//  Copyright © 2017 Mirza Irwan <mirza.irwan.osman@gmail.com>. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class ShoppingList: NSManagedObject {
+    
+}
